e4801edd2ada619cf8a775138389fa305835114f
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-08-21 08:35:36 +0700

    update header compoents

--- src/app/Components/SearchHeader.jsx
+++ src/app/Components/SearchHeader.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 function SearchHeader() {
   return (
-    <div className='bg-gradient-to-r from-purple-500 to-blue-500 h-[300px] flex justify-center items-center text-white'>
-      Search Header
-    </div>
+    <header className='bg-gradient-to-r from-purple-500 to-green-600 h-[300px] flex justify-center items-center '>
+      <div className='text-center'>
+        <h1 className='text-white text-4xl'>Pokemon Finder</h1>
+        <p className='text-white text-lg'>Search Your Pokemon</p>
+        <form action="" className='flex mt-4'>
+          <input
+            type="text"
+            className='w-full px-3 py-2 bg-white rounded-lg shadow-lg'
+            placeholder='Search Pokemon Here'/>
+            <button className='mx-2 px-4 py-2 bg-blue-700 text-white rounded-lg cursor-pointer'>Search</button>
+        </form>
+      </div>
+    </header>
   )
 }
 
