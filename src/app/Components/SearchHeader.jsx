23ca2378ab8c797254ff6fb9df889ca1f44f80ab
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-08-21 08:27:25 +0700

    create header comps

---
+++ src/app/Components/SearchHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchHeader() {
+  return (
+    <div className='bg-gradient-to-r from-purple-500 to-blue-500 h-[300px] flex justify-center items-center text-white'>
+      Search Header
+    </div>
+  )
+}
+
+export default SearchHeader
